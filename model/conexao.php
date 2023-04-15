4c6bbf7854d74848a94a9876fe8a06f5de3c47fd
Verly-G <gabrielverly03@gmail.com>
2023-04-15 09:54:19 -0300

    15.04.23
    Dando início a brincadeira

---
+++ model/conexao.php
@@ -0,0 +1,22 @@
+<?php
+    // Aqui passamos os dados do banco. como qual sintax, local e nome
+    $banco_de_dados = 'mysql:host=localhost;dbname=e-commerce';
+    // Indicando qual é o usuário que terá acesso
+    $user = 'root';
+    // Passando a senha do banco.
+    $password='';
+    // Para tratar os erros, caso aconteça, usaremos o try..catch que é usado na linguagem.
+    try{
+        // Primeiro criamos a conexão passando qual sintax iremos usar, o banco, o nome e a senha de usuário.
+        $conexao = new PDO($banco_de_dados, $user, $password);
+        
+        // Agora usaremos o setAttribute para definir um atributo a conexão. Nesse caso estamos dizendo que
+        // O atributo que será modificado é o modo de tratamento de erros e o atributo que estamos modificando
+        // é uma exceção que será lançada sempre que ocorrer um erro.
+        $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }catch(PDOException $erro){
+        // Passamos a mensagem de erro:
+        echo "Erro: " . $erro->getMessage();
+    }
+
+?>
